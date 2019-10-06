5ef60ae69534bad3c74b9e4a1975c128c1afc58f
virtual-frame-7789 <mttwright@gmail.com>
2019-10-06 15:06:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    
+    if self.find_by_name(name)
+      self.find_by_name
+    else
+      self.create_by_name(name)
+    end
+  end
 
 end
 
